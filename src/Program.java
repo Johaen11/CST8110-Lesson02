59ac88246377c4dc1edcd0eb799d6a57a45e36cf
johng <johng@DESKTOP-31S43F2>
2020-05-12 12:56:01 -0400

    added private name instance variable to program class

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,10 @@
 
 public class Program {
-
+  private static String _name = "";
 	public static void main(String[] args) {
 		System.out.println("Program Started...");
-
+        _name = "Niro";
+        System.out.println(_name);
 	}
 
 }
